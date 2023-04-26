83694a4ad53746a6437a21a14fd12bb67ee27fb9
ria-hue <mumuninasaria@gmail.com>
2023-04-26 10:10:07 -0700

    created a project table

--- database/migrations/2023_04_26_170525_create_projects_table.php
+++ database/migrations/2023_04_26_170525_create_projects_table.php
@@ -11,11 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tasks', function (Blueprint $table) {
+        Schema::create('projects', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('note');
-            $table->string('priority');
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -25,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('projects');
     }
 };
